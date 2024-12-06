2597b99c9c732bb3dfd90b2e7f2030c1fb61adca
ChengNan Yang <chengnan_yang@htc.com>
2024-12-06 20:47:14 +0800

    Rename folder name 3rd Party to ThirdParty

---
+++ VRS_Studio/Assets/ThirdParty/FancyScrollView/Scripts/ItemData.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * FancyScrollView (https://github.com/setchi/FancyScrollView)
+ * Copyright (c) 2020 setchi
+ * Licensed under MIT (https://github.com/setchi/FancyScrollView/blob/master/LICENSE)
+ */
+
+namespace FancyScrollView.Example03
+{
+    class ItemData
+    {
+        public string Message { get; }
+        public MenuType Type { get; }
+
+        public ItemData(string message)
+        {
+            Message = message;
+        }
+
+        public ItemData(string message, MenuType type)
+        {
+            Message = message;
+            Type = type;
+        }
+
+        public enum MenuType
+        {
+            Main,
+            Sub,
+        }
+    }
+}
---
+++ VRS_Studio/Assets/ThirdParty/FancyScrollView/Scripts/MenuPresenter.cs
@@ -0,0 +1,322 @@
+﻿/*
+ * FancyScrollView (https://github.com/setchi/FancyScrollView)
+ * Copyright (c) 2020 setchi
+ * Licensed under MIT (https://github.com/setchi/FancyScrollView/blob/master/LICENSE)
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using VRSStudio.Avatar;
+using VRSStudio.Spectator;
+
+namespace FancyScrollView.Example03
+{
+    public class MenuPresenter : MonoBehaviour
+    {
+        public enum MenuCategory
+        {
+            Main,
+            Spectator,
+            Bodytracking,
+            //PalmMap,
+            Teleport,
+            Menu,
+            Invalid,
+        }
+
+        List<ItemData> mainItemData = new List<ItemData>();
+        List<ItemData> spectatorItemData = new List<ItemData>();
+        List<ItemData> bodytrackingItemData = new List<ItemData>();
+        //List<ItemData> palmMapItemData = new List<ItemData>();
+        List<ItemData> teleportItemData = new List<ItemData>();
+        List<ItemData> menuItemData = new List<ItemData>();
+
+        [SerializeField] ScrollView scrollView = default;
+        [SerializeField] Scroller scroller = default;
+        public AudioClip tutorial;
+
+        private static MenuPresenter instance;
+        public static MenuPresenter Instance { get { return instance; } private set { instance = value; } }
+        private const int MAIN_MENU_MAX_COUNT = 5;
+
+        private int prevSelectedIndex = 0;
+
+        private MenuCategory lastModifiedType = MenuCategory.Invalid;
+
+        private bool prevTracking = false;
+        private string tutorialName = string.Empty;
+        private bool addTutorialItem = false;
+
+        private void Awake()
+        {
+            Instance = this;
+        }
+
+        private void Start()
+        {
+            //mainItemData.Add(new ItemData("Palm Map >", ItemData.MenuType.Main));
+            // If mainItemData size changes, please update MAIN_MENU_MAX_COUNT too
+            mainItemData.Add(new ItemData("Menu >", ItemData.MenuType.Main));
+            mainItemData.Add(new ItemData("Teleport >", ItemData.MenuType.Main));
+            mainItemData.Add(new ItemData("Bodytracking >", ItemData.MenuType.Main));
+            mainItemData.Add(new ItemData("Spectator >", ItemData.MenuType.Main));
+            mainItemData.Add(new ItemData("Reset Tutorial", ItemData.MenuType.Main));
+            StartCoroutine(WaitForUpdate());
+
+            spectatorItemData.Add(new ItemData("< Spectator", ItemData.MenuType.Sub));
+            if (VRSSpectatorManager.Instance)
+            {
+                spectatorItemData.Add(new ItemData("View: " + VRSSpectatorManager.Instance.spectatorMode.ToString(), ItemData.MenuType.Sub));
+            }
+            else
+            {
+                spectatorItemData.Add(new ItemData("View: Headset", ItemData.MenuType.Sub));
+            }
+            spectatorItemData.Add(new ItemData("Panorama", ItemData.MenuType.Sub));
+            spectatorItemData.Add(new ItemData("Tutorial", ItemData.MenuType.Sub));
+            bodytrackingItemData.Add(new ItemData("< Bodytracking", ItemData.MenuType.Sub));
+            bodytrackingItemData.Add(new ItemData("Bodytracking: OFF", ItemData.MenuType.Sub));
+            bodytrackingItemData.Add(new ItemData("Tutorial", ItemData.MenuType.Sub));
+            //palmMapItemData.Add(new ItemData("< Palm Map", ItemData.MenuType.Sub));
+            //palmMapItemData.Add(new ItemData("Tutorial", ItemData.MenuType.Sub));
+            teleportItemData.Add(new ItemData("< Teleport", ItemData.MenuType.Sub));
+            teleportItemData.Add(new ItemData("Tutorial", ItemData.MenuType.Sub));
+            menuItemData.Add(new ItemData("< Menu", ItemData.MenuType.Sub));
+            menuItemData.Add(new ItemData("Tutorial", ItemData.MenuType.Sub));
+        }
+
+        private void OnEnable()
+        {
+            if (lastModifiedType == MenuCategory.Invalid) return;
+            UpdateData();
+        }
+
+        private void OnDisable()
+        {
+            if (lastModifiedType == MenuCategory.Invalid) return;
+            UpdateData();
+        }
+
+        private void LateUpdate()
+        {
+            if (lastModifiedType == MenuCategory.Invalid) return;
+            UpdateData();
+        }
+
+        private IEnumerator WaitForUpdate()
+        {
+            yield return new WaitUntil(() => scrollView != null);
+            scrollView.UpdateData(mainItemData.ToArray());
+            scrollView.SelectCell(0);
+        }
+
+        private void UpdateData()
+        {
+            switch (lastModifiedType)
+            {
+                case MenuCategory.Main:
+                    if (!transform.gameObject.activeSelf) return;
+                    if (addTutorialItem && !string.IsNullOrEmpty(tutorialName))
+                    {
+                        if (mainItemData.Any(x => x.Message.Equals(tutorialName)))
+                        {
+                            tutorialName = string.Empty;
+                            addTutorialItem = false;
+                            break;
+                        }
+                        int index = mainItemData.FindIndex(x => x.Message.Contains("Reset"));
+                        if (index != -1)
+                        {
+                            mainItemData.Insert(index, new ItemData(tutorialName, ItemData.MenuType.Main));
+                            prevSelectedIndex = index;
+                        }
+                        tutorialName = string.Empty;
+                        addTutorialItem = false;
+                    }
+                    scrollView.UpdateData(mainItemData.ToArray());
+                    if (prevSelectedIndex > (mainItemData.Count - 1)
+                        || scroller.Position > (mainItemData.Count - 1))
+                    {
+                        scrollView.SelectCell(mainItemData.Count - 1);
+                        scroller.Position = mainItemData.Count - 1;
+                        prevSelectedIndex = mainItemData.Count - 1;
+                    }
+                    else
+                    {
+                        scrollView.SelectCell(prevSelectedIndex);
+                        scroller.Position = prevSelectedIndex;
+                    }
+                    lastModifiedType = MenuCategory.Invalid;
+                    break;
+                case MenuCategory.Spectator:
+                    scrollView.UpdateData(spectatorItemData.ToArray());
+                    scrollView.SelectCell(1);
+                    scroller.Position = 1;
+                    lastModifiedType = MenuCategory.Invalid;
+                    break;
+                case MenuCategory.Bodytracking:
+                    scrollView.UpdateData(bodytrackingItemData.ToArray());
+                    scrollView.SelectCell(1);
+                    lastModifiedType = MenuCategory.Invalid;
+                    break;
+                //case MenuCategory.PalmMap:
+                //    scrollView.UpdateData(palmMapItemData.ToArray());
+                //    scrollView.SelectCell(1);
+                //    lastModifiedType = MenuCategory.Invalid;
+                //    break;
+                case MenuCategory.Teleport:
+                    scrollView.UpdateData(teleportItemData.ToArray());
+                    scrollView.SelectCell(1);
+                    lastModifiedType = MenuCategory.Invalid;
+                    break;
+                case MenuCategory.Menu:
+                    scrollView.UpdateData(menuItemData.ToArray());
+                    scrollView.SelectCell(1);
+                    lastModifiedType = MenuCategory.Invalid;
+                    break;
+            }
+        }
+
+        public void AddSpectatorResetCameraViewItem()
+        {
+            lastModifiedType = MenuCategory.Spectator;
+            spectatorItemData.Insert(3, new ItemData("Reset Camera", ItemData.MenuType.Sub));
+        }
+
+        public void RemoveSpectatorResetCameraViewItem()
+        {
+            lastModifiedType = MenuCategory.Spectator;
+            spectatorItemData.RemoveAt(3);
+        }
+
+        public void UpdateSpectatorViewItem(string msg)
+        {
+            lastModifiedType = MenuCategory.Spectator;
+            spectatorItemData.Insert(1, new ItemData(msg, ItemData.MenuType.Sub));
+            spectatorItemData.RemoveAt(2);
+        }
+
+        public void ShowSubmenu()
+        {
+            if (scrollView.GetSelectedItemData().Message.Contains("Spectator"))
+            {
+                lastModifiedType = MenuCategory.Spectator;
+            }
+            else if (scrollView.GetSelectedItemData().Message.Contains("Bodytracking"))
+            {
+                lastModifiedType = MenuCategory.Bodytracking;
+            }
+            //else if (scrollView.GetSelectedItemData().Message.Contains("Palm Map"))
+            //{
+            //    prevSelectedIndex = scrollView.GetSelectedIndex();
+            //    lastModifiedType = MenuCategory.PalmMap;
+            //}
+            else if (scrollView.GetSelectedItemData().Message.Contains("Teleport"))
+            {
+                lastModifiedType = MenuCategory.Teleport;
+            }
+            else if (scrollView.GetSelectedItemData().Message.Contains("Menu"))
+            {
+                lastModifiedType = MenuCategory.Menu;
+            }
+        }
+
+        public void ShowSpectatorSubmenu()
+        {
+            lastModifiedType = MenuCategory.Spectator;
+        }
+
+        public void BackToMainMenu()
+        {
+            lastModifiedType = MenuCategory.Main;
+        }
+
+        public bool IsSpectatorSubmenu()
+        {
+            if (scrollView.GetFirstItemData().Message.Contains("Spectator") && scrollView.GetFirstItemData().Type == ItemData.MenuType.Sub) return true;
+
+            return false;
+        }
+
+        public bool IsTeleportSubmenu()
+        {
+            if (scrollView.GetFirstItemData().Message.Contains("Teleport") && scrollView.GetFirstItemData().Type == ItemData.MenuType.Sub) return true;
+
+            return false;
+        }
+
+        public bool IsInSubmenu()
+        {
+            if (scrollView.GetFirstItemData().Type == ItemData.MenuType.Sub) return true;
+
+            return false;
+        }
+
+        public void AddTutorialViewItem(string msg)
+        {
+            tutorialName = msg;
+            addTutorialItem = true;
+            lastModifiedType = MenuCategory.Main;
+        }
+
+        public void RemoveTutorialViewItem()
+        {
+            while (mainItemData.Count > MAIN_MENU_MAX_COUNT)
+            {
+                int index1 = mainItemData.FindIndex(x => x.Message.Contains("Spectator"));
+                int index2 = mainItemData.FindIndex(x => x.Message.Contains("Reset"));
+                int startIndex = index1 + 1;
+                if (index1 != -1 && index2 != -1)
+                {
+                    mainItemData.RemoveRange(startIndex, index2 - startIndex);
+                }
+            }
+            lastModifiedType = MenuCategory.Main;
+        }
+
+        public void UpdateMainMenu()
+        {
+            if (mainItemData.Count <= 0) { return; }
+            scrollView.UpdateData(mainItemData.ToArray());
+
+            if (prevSelectedIndex > (mainItemData.Count - 1)
+                        || scroller.Position > (mainItemData.Count - 1))
+            {
+                scrollView.SelectCell(mainItemData.Count - 1);
+                scroller.Position = mainItemData.Count - 1;
+                prevSelectedIndex = mainItemData.Count - 1;
+            }
+            else
+            {
+                scrollView.SelectCell(prevSelectedIndex);
+                scroller.Position = prevSelectedIndex;
+            }
+        }
+
+        public void UpdateBodytrackingStatus()
+        {
+            if (VRSBodyTrackingManager.Instance == null ||
+                prevTracking == VRSBodyTrackingManager.Instance.IsTracking()) { return; }
+
+            lastModifiedType = MenuCategory.Bodytracking;
+            if (VRSBodyTrackingManager.Instance.IsTracking())
+            {
+                bodytrackingItemData.Insert(1, new ItemData("Bodytracking: ON", ItemData.MenuType.Sub));
+            }
+            else
+            {
+                bodytrackingItemData.Insert(1, new ItemData("Bodytracking: OFF", ItemData.MenuType.Sub));
+            }
+            bodytrackingItemData.RemoveAt(2);
+            scroller.Position = 2;
+            prevTracking = VRSBodyTrackingManager.Instance.IsTracking();
+        }
+
+        public void SetPrevSelectedIndex(int idx)
+        {
+            prevSelectedIndex = idx;
+        }
+    }
+}
